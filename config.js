dc7d9f6a00ade6f94abceda59d19917b4da565a2
ravishkadisanayaka <141713471+ravishkadisanayaka@users.noreply.github.com>
2025-01-01 17:16:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "bqY1zbwJ#J5LzN-vTitc2kUuC9Bf6WIxmkzYcHcAf0lwbB5jtqPM", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
